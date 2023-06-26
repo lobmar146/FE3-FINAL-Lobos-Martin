01bc034c9e67f57434c5cf27e7dcc5f25bfb92fc
lobmar146 <lobmar146@gmail.com>
2023-06-26 17:17:32 -0300

    Agregando Contact

---
+++ src/components/contact/FormularioContacto.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+import ListaErrores from './ListaErrores'
+import generarId from './Autoid.js'
+import { SiWolframlanguage } from 'react-icons/si'
+export default function FormularioContacto() {
+  const [nombre, setNombre] = useState('')
+  const [mail, setMail] = useState('')
+  const [error, setError] = useState([])
+  const [exito, setExito] = useState(false)
+
+  const agregarError = errorNuevo => {
+    setError(prevErrors => [
+      ...prevErrors,
+      { nombreError: errorNuevo, id: generarId() }
+    ])
+  }
+  function esCorreoElectronico(cadena) {
+    // Expresión regular para verificar el formato del correo electrónico
+    var patron = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    // Utiliza el método test() de la expresión regular para verificar si la cadena cumple el patrón
+    return patron.test(cadena)
+  }
+
+  const validarFormulario = () => {
+    let resultado = true
+    setError([])
+
+    if (nombre.trim().length < 5) {
+      agregarError('El nombre debe tener al menos 5caracteres')
+      resultado = false
+    }
+    if (!esCorreoElectronico(mail)) {
+      agregarError('El mail no es valido')
+      resultado = false
+    }
+    setExito(resultado)
+    return resultado
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (validarFormulario()) {
+      setExito(true)
+      setMail('')
+      setNombre('')
+    }
+  }
+
+  const changeNombre = e => {
+    setNombre(e.target.value)
+  }
+
+  const changeMail = e => {
+    setMail(e.target.value)
+  }
+
+  return (
+    <section className='section-form'>
+      <h2>Escribenos. Pronto un miembro de la manda se pondra en contacto </h2>
+
+      <form onSubmit={handleSubmit}>
+        {' '}
+        {error.length === 0 ? undefined : <ListaErrores errores={error} />}
+        <label htmlFor='nombre'>Nombre</label>
+        <input type='text' id='nombre' value={nombre} onChange={changeNombre} />
+        <label htmlFor='mail'>Mail</label>
+        <input type='text' id='mail' value={mail} onChange={changeMail} />
+        <button type='submit'>Enviar</button>
+        {exito && (
+          <p className='exito'>
+            Gracias por comunicarse. En la brevedad le responderemos
+          </p>
+        )}
+      </form>
+    </section>
+  )
+}
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,3 +1,14 @@
+import FormularioContacto from '../components/contact/FormularioContacto'
+import { GiDirewolf } from 'react-icons/gi'
 export default function Contact() {
-  return <h1>Contact</h1>
+  return (
+    <section classname='section-contact'>
+      <h1>
+        Ponte en contacto con la manada <GiDirewolf className='h1' />{' '}
+      </h1>
+      <div className='contenedor-contact'>
+        <FormularioContacto />
+      </div>
+    </section>
+  )
 }
